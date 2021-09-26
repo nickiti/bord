1df578ba8ede3307f1490ab1ac4a94f8b4bad81c
Nickiti <nickwin1999@yahoo.com>
2021-09-26 14:12:21 -0500

    security

--- src/API_Files/YelpFetch.js
+++ src/API_Files/YelpFetch.js
@@ -26,11 +26,10 @@ import queryString from 'query-string';
 
 export function get(path, queryParams){
   const API_URL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3"
-  const API_KEY = "P8YLpzcGfGYZv_Y3ST3RH0iEL_WzBmIuwwxH80RvaucZbzGygweZqPMGbuixnWUB2iy4y9W0rxPqTLcw1ZC3vxJSCLHlXptq7F6OMGvpg1ZqezPoVp-tE7hJzwdNYXYx"
   const query = queryString.stringify(queryParams);
   return fetch(`${API_URL}${path}?${query}`, {
             headers: {
-                 Authorization: `Bearer ${API_KEY}`,
+                 Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
                  Origin: 'localhost',
                  withCredentials: true,
 
